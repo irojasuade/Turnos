2e630b2a129eb8eccab36aee8d47faff88660d0a
irojasuade <irojas@uade.edu.ar>
2025-06-04 10:14:20 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const turnosRouter = require('./routes/turnos');
+
+app.use(cors());
+app.use(express.json());
+app.use('/turnos', turnosRouter);
+
+app.get('/', (req, res) => {
+  res.send('Gestor de turnos funcionando ✅');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en puerto ${port}`);
+});
